39d0ce8eed03d2f9f2d86159805b4a2f46e2e28c
Marianopenn <marianopenn@gmail.com>
2015-04-27 17:23:20 -0400

    Added Graph class

---
+++ Task 1/Graph.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+        private Map<Integer, List<Integer>> adjacencyList;
+        
+        public Graph() {
+            adjacencyList = new HashMap<Integer, List<Integer>>();
+            
+            
+        }
+        private class edge() {
+            
+        }
+    }
